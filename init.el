1cd16a38f727d94b193aa4e21b266b6b29904e8f
awc <aaron.ceross@gmail.com>
2016-05-07 18:17:11 +0100

    My first emacs init file commit

---
+++ init.el
@@ -0,0 +1,273 @@
+;;;; init.el
+
+(setq user-full-name "Aaron Ceross")
+(require 'cl)
+
+(load "package")
+(package-initialize)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(setq package-archive-enable-alist '(("melpa" deft magit)))
+
+(defvar my-packages '(ac-slime
+		      airline-themes
+		      autopair
+		      clojure-mode
+		      company-auctex
+		      company-inf-ruby
+		      company-irony
+		      company-irony-c-headers
+		      company-tern
+		      deft
+		      diminish
+		      ein
+		      elpy
+		      flycheck
+		      git-gutter-fringe
+		      haskell-mode
+		      js2-mode
+		      highlight-indentation
+		      ido-ubiquitous
+		      ido-vertical-mode
+		      indent-guide
+		      latex-preview-pane
+		      magit
+		      markdown-mode
+		      marmalade
+		      material-theme
+		      nodejs-repl
+		      org
+		      paredit
+		      powerline
+		      py-autopep8
+		      rainbow-delimiters
+		      smex
+		      sml-mode
+		      smooth-scrolling
+		      web-mode
+		      writegood-mode
+		      yaml-mode
+		      ztree)
+  "default packages")
+
+(defun my-packages-installed-p ()
+  (loop for pkg in my-packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+(unless (my-packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg my-packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+;;; UI Customisations -----------------------------------------------------------
+
+;; load themes
+(load-theme 'material t)
+
+;; powerline
+(require 'powerline)
+;; (powerline-default-theme)
+
+;; airline modeline theme
+(require 'airline-themes)
+(load-theme 'airline-molokai t)
+(setq airline-utf-glyph-separator-left      #xe0b0
+      airline-utf-glyph-separator-right     #xe0b2
+      airline-utf-glyph-subseparator-left   #xe0b1
+      airline-utf-glyph-subseparator-right  #xe0b3
+      airline-utf-glyph-branch              #xe0a0
+      airline-utf-glyph-readonly            #xe0a2
+      airline-utf-glyph-linenumber          #xe0a1)
+
+;; remove the splash screen
+(setq inhibit-splash-screen t)
+
+;; remove the scroll bar
+(scroll-bar-mode -1)
+
+;; remove the tool bar
+(tool-bar-mode -1)
+
+;; remove the menu
+(menu-bar-mode -1)
+
+;; set number lines globally
+(global-linum-mode t)
+
+;; provide column numbers
+(setq column-number-mode t)
+
+;; default size of the emacs window
+(setq initial-frame-alist '((top . 0) (left . 0) (width . 110) (height . 40)))
+
+(setq-default indicate-empty-lines t)
+(when (not indicate-empty-lines)
+  (toggle-indicate-empty-lines))
+
+;; full path in title bar
+(setq-default frame-title-format "%b (%f)")
+
+;; indent guide, shows a line to indicate which block currently editing
+(require 'indent-guide)
+(indent-guide-global-mode)
+(setq indent-guide-char "|")
+(set-face-foreground 'indent-guide-face "pink1")
+
+;; highlight indentation
+(require 'highlight-indentation)
+(add-hook 'c-mode-hook 'highlight-indentation-mode)
+(add-hook 'c++-mode-hook 'highlight-indentation-mode)
+(add-hook 'emacs-lisp-mode 'highlight-indentation-mode)
+(add-hook 'python-mode-hook 'highlight-indentation-mode)
+(add-hook 'java-mode-hook 'highlight-indentation-mode)
+(add-hook 'clojure-mode-hook 'highlight-indentation-mode)
+(add-hook 'lisp-mode-hook 'highlight-indentation-mode)
+(add-hook 'js-mode-hook 'highlight-indentation-mode)
+
+;; delimiters in different colours
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+;; ido vertical mode
+(require 'ido-vertical-mode)
+(ido-mode 1)
+(ido-vertical-mode 1)
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
+;; show git diff in the buffer
+(require 'git-gutter-fringe)
+(global-git-gutter-mode t)
+
+(defadvice vc-git-mode-line-string (after plus-minus (file) compile activate)
+  (setq ad-return-value
+    (concat ad-return-value
+            (let ((plus-minus (vc-git--run-command-string
+                               file "diff" "--numstat" "--")))
+              (and plus-minus
+                   (string-match "^\\([0-9]+\\)\t\\([0-9]+\\)\t" plus-minus)
+                   (format " +%s-%s" (match-string 1 plus-minus) (match-string 2 plus-minus)))))))
+
+;;; Editing customisations ------------------------------------------------------
+
+;; comments
+(defun toggle-comment-on-line ()
+  "comment or uncomment current line"
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+(global-set-key (kbd "C-;") 'toggle-comment-on-line)
+
+;; highlight current line
+(global-hl-line-mode 1)
+
+;; highlight matching parenthesis
+(show-paren-mode 1)
+
+(delete-selection-mode t)
+(transient-mark-mode t)
+(setq x-select-enable-clipboard t
+      x-select-enable-primary t)
+
+(setq tab-width 2
+      indent-tabs-mode nil)
+
+(setq make-backup-files nil)
+
+;; change all the yes-or-no answers to simple y or p
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; filterable list of possible commands in minibuffer
+(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; ido 
+(ido-mode t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers t)
+; only try to match within the work directory
+(setq ido-auto-merge-work-directories-length -1)
+
+(require 'autopair)
+(autopair-global-mode)
+
+;;;; Language specific settings -------------------------------------------------
+
+;;; company-mode autocomplete
+(add-hook 'after-init-hook 'global-company-mode)
+(eval-after-load 'company
+  '(add-to-list 'company-backends '(company-irony-c-headers
+                                    company-irony
+                                    company-tern
+                                    company-jedi
+                                    company-inf-ruby
+                                    company-web-html)))
+
+;;; Lisp and clojure
+(setq lisp-modes '(lisp-mode
+                   emacs-lisp-mode
+                   common-lisp-mode
+                   scheme-mode
+                   clojure-mode))
+
+;;; C/C++
+(require 'company-irony-c-headers)
+
+;;; Python
+(elpy-enable)
+(elpy-use-ipython)
+
+;; use flycheck and not flymake
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; enable autopep8 formatting on save
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+;;; Javascript
+
+;;; LaTex
+(require 'company-auctex)
+(company-auctex-init)
+(setq TeX-parse-self t) ; Enable parse on load.
+(setq TeX-auto-save t)  ; Enable parse on save.
+
+;; init.el ends here
+	   
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(airline-display-directory (quote airline-directory-shortened))
+ '(airline-shortened-directory-length 20)
+ '(package-selected-packages
+   (quote
+    (minimap yaml-mode writegood-mode web-mode sml-mode smex paredit nodejs-repl material-theme marmalade markdown-mode magit haskell-mode flycheck elpy deft clojure-mode autopair ac-slime))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;; diminish -------------------------------------------------------------------
+
+;; remove cruft from modeline
+(require 'diminish)
+(diminish 'projectile-mode)
+(diminish 'flycheck-mode)
+(diminish 'paredit-mode)
+(diminish 'autopair-mode)
+(diminish 'indent-guide-mode)
+(diminish 'git-gutter-mode)
+(diminish 'abbrev-mode)
+(diminish 'highlight-indentation-mode)
