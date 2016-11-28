82333cd0c0e43d3aceb360169dfaa0038201f0b3
awc <aaron.ceross@gmail.com>
2016-11-28 08:11:46 +0000

    Fixing some of the ido configs

--- config/navigation.el
+++ config/navigation.el
@@ -12,14 +12,12 @@
 
 ;; ido
 (use-package ido
+  :ensure t
   :config
-  (ido-mode t)
- ; (flx-ido-mode t)
   (setq ido-enable-flex-matching t)
-  (setq ido-everywhere t)
+  (setq ido-everywhere)
+  (ido-mode)
   (setq ido-use-virtual-buffers t) ; only try to match within the work directory
-   ;; use up and down to scroll through ido instead of left-right
-  (setq ido-vertical-define-keys 'C-n-C-p-up-and-down)
   (setq ido-max-prospects 50)
   (setq ido-max-window-height 0.25)
   (setq ido-auto-merge-work-directories-length -1)
@@ -27,16 +25,21 @@
   (global-set-key (kbd "C-x C-b") 'ibuffer)
   )
 
+(use-package flx-ido
+  :config
+  (flx-ido-mode))
+
+(use-package ido-ubiquitous
+  :ensure t
+  :config (ido-ubiquitous-mode 1))
+
 (use-package ido-vertical-mode
   :ensure t
   :config
   (ido-vertical-mode)
+  ;; use up and down to scroll through ido instead of left-right
   (setq ido-vertical-define-keys 'C-n-and-C-p-only)
   )
-
-(use-package ido-ubiquitous
-  :ensure t
-  :config (ido-ubiquitous-mode))
 
 ;; Windmove configuration
 (when (fboundp 'windmove-default-keybindings)
