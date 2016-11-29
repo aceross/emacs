e360705307b01096be66fbcaa2676b456096b29b
awc <aaron.ceross@gmail.com>
2016-11-29 10:37:33 +0000

    Adding some ESS goodness

---
+++ config/init-ess.el
@@ -0,0 +1,15 @@
+;;; init-ess.el --- Customisations for the Emacs Speaks Statistics package
+;;
+;;; Commentary:
+;;
+
+;;; Code:
+
+(use-package ess
+  :ensure t
+  :defer t
+  :commands R)
+
+(provide 'init-ess)
+
+;;; init-ess.el ends here
