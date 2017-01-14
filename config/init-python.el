a8ae07bb1d69d85b0929be337e4f212c6d828db0
awc <aaron.ceross@gmail.com>
2017-01-14 10:10:21 +0000

    Update to python-config, ipython still broken

--- config/init-python.el
+++ config/init-python.el
@@ -22,32 +22,13 @@
       (remove-hook 'elpy-modules 'elpy-module-yasnippet)
       (remove-hook 'elpy-mode-hook 'elpy-module-highlight-indentation)
       (add-hook 'elpy-mode-hook 'flycheck-mode))
-    (setq py-python-command "/usr/bin/python3")
+    (setq elpy-rpc-python-command "python3")
     (setq python-shell-interpreter "ipython3"
-	  python-shell-interpreter-args "--simple-prompt --pprint")
-    (elpy-enable)
-    (elpy-use-ipython)
+          python-shell-interpreter-args "--simple-prompt --pprint")
     (add-hook 'elpy-mode-hook 'elpy-use-ipython "ipython3")
-    ;; jedi is great
     (setq elpy-rpc-backend "jedi")
+    (elpy-enable)
     ))
-
-;; (use-package elpy
-;;   :ensure t
-;;   :config
-;;   (remove-hook 'elpy-modules 'elpy-module-flymake)
-;;   (remove-hook 'elpy-modules 'elpy-module-yasnippet)
-;;   (setq elpy-rpc-backend "jedi"
-;;      elpy-rpc-python-command "python3")
-;;   (setq py-python-command "/usr/bin/python3")
-;;   (setq python-shell-interpreter "ipython3"
-;;      python-shell-interpreter-args "--simple-prompt --pprint")
-;;   (elpy-use-ipython)
-;;   (add-hook 'elpy-mode-hook 'elpy-use-ipython "ipython3")
-;;   (add-hook 'python-mode-hook 'jedi:setup)
-;;   (setq jedi:complete-on-dot t)
-;;   (elpy-enable)
-;;   )
 
 (use-package flycheck-pyflakes
     :ensure t
