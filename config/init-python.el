860d647ec1c18be8ed40b173b6de4ac430ea7272
awc <aaron.ceross@gmail.com>
2017-04-09 12:17:15 +0100

    Fixed some errors in python-mode (garbled interpreter)

--- config/init-python.el
+++ config/init-python.el
@@ -2,18 +2,20 @@
 ;;
 ;;; Commentary:
 ;;
-
+;;   Interactive python session and completion.
+;;
 ;;; Code:
 
 (use-package python :ensure t)
 
 (use-package company-jedi
   :ensure t
+  :defer t
   :config (add-to-list 'company-backends 'company-jedi))
 
 (use-package elpy
   :ensure t
-  :defer 2
+  :defer t
   :config
   (progn
     ;; Use Flycheck instead of Flymake
@@ -22,17 +24,19 @@
       (remove-hook 'elpy-modules 'elpy-module-yasnippet)
       (remove-hook 'elpy-mode-hook 'elpy-module-highlight-indentation)
       (add-hook 'elpy-mode-hook 'flycheck-mode))
+    ;; Default to Python 3 instead of Python 2
     (setq elpy-rpc-python-command "python3")
     (setq python-shell-interpreter "ipython3"
-          python-shell-interpreter-args "--simple-prompt --pprint")
+          python-shell-interpreter-args "--simple-prompt -i")
     (add-hook 'elpy-mode-hook 'elpy-use-ipython "ipython3")
     (setq elpy-rpc-backend "jedi")
     (elpy-enable)
     ))
 
 (use-package flycheck-pyflakes
-    :ensure t
-    :config (add-hook 'python-mode-hook 'flycheck-mode))
+  :ensure t
+  :defer t
+  :config (add-hook 'python-mode-hook 'flycheck-mode))
 
 (use-package py-autopep8
   :ensure t
