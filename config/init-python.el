cc0732a319312024c38d503762ebe3444c945327
awc <aaron.ceross@gmail.com>
2017-05-29 21:05:47 +0100

    Fixed errors in iPython
    Now has pretty printing as well as cleaned up the cruft of code that
    didn't do much.

--- config/init-python.el
+++ config/init-python.el
@@ -15,26 +15,23 @@
 
 (use-package elpy
   :ensure t
-  :defer t
   :config
-  (progn
-    ;; Use Flycheck instead of Flymake
-    (when (require 'flycheck nil t)
-      (remove-hook 'elpy-modules 'elpy-module-flymake)
-      (remove-hook 'elpy-modules 'elpy-module-yasnippet)
-      (remove-hook 'elpy-mode-hook 'elpy-module-highlight-indentation)
-      (add-hook 'elpy-mode-hook 'flycheck-mode))
-    ;; Default to Python 3 instead of Python 2
-    (add-hook 'python-mode-hook 'elpy-mode)
-    (setq py-python-command "/usr/bin/python3")
-    (elpy-enable)
-    (elpy-use-ipython "ipython3")
-    (setq elpy-rpc-python-command "python3")
-    (setq python-shell-interpreter "ipython3")
-    (setq python-shell-interpreter-args "--simple-prompt -pprint")
-    (setq elpy-rpc-backend "jedi")
-    (setq jedi:complete-on-dot t)
-    ))
+  ;; Use Flycheck instead of Flymake
+  (when (require 'flycheck nil t)
+    (remove-hook 'elpy-modules 'elpy-module-flymake)
+    (remove-hook 'elpy-modules 'elpy-module-yasnippet)
+    (remove-hook 'elpy-mode-hook 'elpy-module-highlight-indentation)
+    (add-hook 'elpy-mode-hook 'flycheck-mode))
+  (elpy-enable)
+  (elpy-use-ipython)
+  (setq py-python-command "/usr/bin/python3")
+  (setq elpy-rpc-python-command "python3")
+  (setq python-shell-interpreter "ipython3")
+  (setq python-shell-interpreter-args "--simple-prompt")
+  (setq elpy-rpc-backend "jedi")
+  (setq jedi:complete-on-dot t)
+  )
+
 
 (use-package flycheck-pyflakes
   :ensure t
