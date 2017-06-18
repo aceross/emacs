f8f810b411130b442a92338220d12b9b7e5759c7
awc <aaron.ceross@gmail.com>
2017-06-18 21:20:17 +0100

    Nodejs repl added
    Always nice to have a repl

--- config/init-javascript.el
+++ config/init-javascript.el
@@ -35,6 +35,18 @@
 
   (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
 
+(use-package nodejs-repl
+  :ensure t
+  :defer t
+  :config
+  (add-hook 'js-mode-hook
+          (lambda ()
+            (define-key js-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-sexp)
+            (define-key js-mode-map (kbd "C-c C-r") 'nodejs-repl-send-region)
+            (define-key js-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
+            (define-key js-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
+  )
+
 (use-package color-identifiers-mode
   :ensure t
   :defer t
