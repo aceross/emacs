5c0867a81bbd07000de26fefb8a78cb22ec688cd
awc <aaron.ceross@gmail.com>
2025-01-09 10:54:21 +0000

    Add `pyright` to lsp configuration

--- site-elisp/init-lsp.el
+++ site-elisp/init-lsp.el
@@ -31,6 +31,7 @@
 
 (use-package lsp-pyright
   :ensure t
+  :custom (lsp-pyright-langserver-command "pyright")
   :hook (python-mode . (lambda ()
                           (require 'lsp-pyright)
                           (lsp-deferred)))
