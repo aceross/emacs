e38fb1321044f7a85d0c819a95ff8093f8a0aad3
awc <aaron.ceross@gmail.com>
2025-11-19 06:18:20 +0000

    Clean up lsp-mode

--- site-elisp/init-lsp.el
+++ site-elisp/init-lsp.el
@@ -1,60 +1,65 @@
+;;; init-lsp.el --- LSP configuration -*- lexical-binding: t; -*-
+
+;;; Code:
+
 (use-package lsp-mode
   :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l")
-  (setq lsp-enable-snippet nil)
+  ;; Prefix for `lsp-command-map'
+  (setq lsp-keymap-prefix "C-c l"
+        lsp-enable-snippet nil          ;; we use yasnippet directly
+        lsp-prefer-flymake nil          ;; use flycheck instead
+        lsp-idle-delay 0.3)
   :hook
-  ((python-mode . lsp)
-   (ess-mode    . lsp)
-   (LaTeX-mode  . lsp)
-   (c-mode      . lsp)
-   (c++-mode    . lsp)
-   (julia-mode  . lsp)
-   (lsp-mode    . lsp-enable-which-key-integration))
-  :commands lsp)
+  ;; Let lsp-pyright handle python-mode; don't hook python here.
+  ((ess-r-mode . lsp-deferred)
+   (LaTeX-mode . lsp-deferred)
+   (c-mode     . lsp-deferred)
+   (c++-mode   . lsp-deferred)
+   (julia-mode . lsp-deferred)
+   (lsp-mode   . lsp-enable-which-key-integration))
+  :commands (lsp lsp-deferred))
 
 (use-package lsp-ui
+  :after lsp-mode
   :commands lsp-ui-mode
-  :hook
-  ((lsp-mode . lsp-ui-mode))
-  :bind
-  (("C-h ." . lsp-ui-doc-focus-frame))
+  :hook (lsp-mode . lsp-ui-mode)
+  :bind (("C-h ." . lsp-ui-doc-focus-frame))
   :custom
   (lsp-ui-doc-enable t)
-  ;; (lsp-ui-header t)
-  ;; (lsp-eldoc-enable-hover t)
-  (lsp-doc-use-childframe t)
+  (lsp-ui-doc-use-childframe t)
   (lsp-ui-doc-use-webkit t)
-  (lsp-ui-peek-enable t)
-  ;; (lsp-ui-doc-position 'bottom)
-  )
+  (lsp-ui-peek-enable t))
 
+;; Python: use Pyright
 (use-package lsp-pyright
+  :after lsp-mode
   :ensure t
-  :custom (lsp-pyright-langserver-command "pyright")
-  :hook (python-mode . (lambda ()
-                          (require 'lsp-pyright)
-                          (lsp-deferred)))
-  :config
-  ;; set this to nil if getting too many false positive type errors
-  (setq lsp-pyright-use-library-code-for-types t)
-  (setq lsp-pyright-stub-path (concat (getenv "HOME") "/src/python-type-stubs"))
-  )
+  :custom
+  ;; adjust if needed
+  (lsp-pyright-langserver-command "pyright")
+  (lsp-pyright-use-library-code-for-types t)
+  (lsp-pyright-stub-path (concat (getenv "HOME") "/src/python-type-stubs"))
+  :hook
+  (python-mode . (lambda ()
+                   (require 'lsp-pyright)
+                   (lsp-deferred))))
 
-(use-package lsp-julia)
+;; Julia
+(use-package lsp-julia
+  :after lsp-mode)
 
+;; Grammar / spell / style checking via LTeX
 (use-package lsp-ltex
   :after lsp-mode
   :ensure t
   :custom
   (lsp-ltex-language "en-GB")
-  :hook (text-mode . (lambda ()
-                       (require 'lsp-ltex)
-                       (lsp)))  ; or lsp-deferred
-  ;; :init
-  ;; (setq lsp-ltex-version "18.4.0")
-  )  ; make sure you have set this
+  :hook
+  (text-mode . (lambda ()
+                 (require 'lsp-ltex)
+                 (lsp-deferred))))
 
+;; Snippets
 (use-package yasnippet
   :hook
   (prog-mode . yas-minor-mode)
@@ -66,7 +71,8 @@
   (yas-reload-all)
   (setq yas-snippet-dirs '("~/.emacs.d/snippets")))
 
-(use-package yasnippet-snippets)
+(use-package yasnippet-snippets
+  :after yasnippet)
 
 (provide 'init-lsp)
 
